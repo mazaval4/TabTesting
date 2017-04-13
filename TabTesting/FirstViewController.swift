2ab69888949c7a68768079e67e052f6cae2757fc
armoren4 <arianna.moreno@asu.edu>
2017-04-13 14:29:47 -0700

    Fixing the Home Tab
    Changed from a tableview to a regular view in the FirstViewController.
    Added: vibration button, lights button, sound button, respective
    actions with each button, fixed the UITextField to only provide the
    number keyboard and dismiss the keyboard, an empty bluetooth connection
    view and calibration view to storyboard

--- TabTesting/FirstViewController.swift
+++ TabTesting/FirstViewController.swift
@@ -7,65 +7,119 @@
 //
 
 import UIKit
-class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+import AudioToolbox
+import AVFoundation
 
-
-    @IBOutlet weak var tableView: UITableView!
-    var items: [String] = ["We", "Heart", "StepPlus"]
-
+class FirstViewController: UIViewController, UITextFieldDelegate {
+    
+    var pressed1 = false
+    var pressed2 = false
+    var pressed3 = false
+    let soundID: SystemSoundID = 1016
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-     //   self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-       //
-        tableView.rowHeight = UITableViewAutomaticDimension
-        tableView.estimatedRowHeight = 500
         setupNavigationBarItems()
-    }
-
-// START TABLE VIEW STUFF
-    
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //return self.items.count;
-        return 2;
-    }
-
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "TextInputCell") as! TextInputTableViewCell
-            cell.configure(text: "", placeholder: "#")
-            return cell
+        insertStepAlertGoal(text: "", placeholder: "#")
         }
-        else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "FeedbackSelection") as! FeedbackTypesTableViewCell
-            // set data for cell here
-            return cell
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-    }
-    
-    private func setupNavigationBarItems () {
-        
-        let titleImageView = UIImageView(image: #imageLiteral(resourceName: "StepPlusHeader"))
-        titleImageView.frame = CGRect(x:0, y:0, width: 60, height: 60)
-        titleImageView.contentMode = .scaleAspectFit
-        navigationItem.titleView = titleImageView
-        
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // MARK: Navigation Bar Stuff
+    
+    private func setupNavigationBarItems () {
+        let titleImageView = UIImageView(image: #imageLiteral(resourceName: "StepPlusHeader"))
+        titleImageView.frame = CGRect(x:0, y:0, width: 60, height: 60)
+        titleImageView.contentMode = .scaleAspectFit
+        navigationItem.titleView = titleImageView
+    }
+
+    
+   // MARK: Today's Goals
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    func insertStepAlertGoal(text: String?, placeholder: String) {
+        textField.text = text
+        textField.placeholder = placeholder
+        textField.accessibilityValue = text
+        textField.accessibilityLabel = placeholder
+        
+    // DISMISS KEYBOARD ACTIONS
+        // click anywhere off the keyboard
+            self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
+        
+        // click the done button
+            //init toolbar
+            let toolbar:UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 30))
+            //create left side empty space so that done button set on right side
+            let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+            let doneBtn: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(FirstViewController.doneButtonAction))
+            //array of BarButtonItems
+            var arr = [UIBarButtonItem]()
+            arr.append(flexSpace)
+            arr.append(doneBtn)
+            toolbar.setItems(arr, animated: false)
+            toolbar.sizeToFit()
+        
+            //setting toolbar as inputAccessoryView
+            self.textField.inputAccessoryView = toolbar
+    }
+        // continue dismiss keyboard
+        func doneButtonAction() {
+            self.view.endEditing(true)
+        }
+    
+    
+    // MARK: Feedback Selection
+
+    @IBAction func VButton(_ sender: UIButton) {
+        if !pressed1 {
+            pressed1 = true
+            sender.setImage(#imageLiteral(resourceName: "graybutton"), for:UIControlState.normal)
+        } else {
+            pressed1 = false
+            sender.setImage(#imageLiteral(resourceName: "checkbutton"), for:UIControlState.normal)
+            AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
+        }
+    }
+    @IBAction func SButton(_ sender: UIButton) {
+        if !pressed2 {
+            pressed2 = true
+            sender.setImage(#imageLiteral(resourceName: "graybutton"), for:UIControlState.normal)
+        } else {
+            pressed2 = false
+            sender.setImage(#imageLiteral(resourceName: "checkbutton"), for:UIControlState.normal)
+            AudioServicesPlaySystemSound(1326)
+        }
+    }
+    @IBAction func LButton(_ sender: UIButton) {
+        if !pressed3 {
+            pressed3 = true
+            sender.setImage(#imageLiteral(resourceName: "graybutton"), for:UIControlState.normal)
+        } else {
+            pressed3 = false
+            sender.setImage(#imageLiteral(resourceName: "checkbutton"), for:UIControlState.normal)
+            blinkscreen()
+        }
+    }
+    
+    func blinkscreen(){
+    if let wnd = self.view {
+        var v = UIView(frame: wnd.bounds)
+        v.backgroundColor = UIColor(red: 0.44, green: 0.87, blue:0.83, alpha:1.0)
+        v.alpha = 1
+        
+        wnd.addSubview(v)
+        UIView.animate(withDuration: 0.5, animations: { v.alpha = 0.0}, completion: {(finished:Bool) in
+            print("inside")
+            v.removeFromSuperview() })
+        }
+    }
+    
 }
 
